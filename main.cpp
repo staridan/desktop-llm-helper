54c169f30febfd937dae5bfa6189e0d46541c6af
a.vinogradov <a.vinogradov@bpcbt.com>
2025-05-18 22:59:07 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QLockFile>
+#include <QStandardPaths>
+#include <QDir>
+#include <QMessageBox>
+#include <QObject>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    // Блокировка единственного экземпляра через QLockFile
+    QString tmpDir = QStandardPaths::writableLocation(QStandardPaths::TempLocation);
+    QDir().mkpath(tmpDir);
+    QLockFile lockFile(tmpDir + QDir::separator() + "TextHelper.lock");
+    lockFile.setStaleLockTime(0);
+    if (!lockFile.tryLock()) {
+        QMessageBox::warning(nullptr,
+                             QObject::tr("TextHelper"),
+                             QObject::tr("Другая копия TextHelper уже запущена."));
+        return 0;
+    }
+
     a.setQuitOnLastWindowClosed(false);
 
     MainWindow w;
